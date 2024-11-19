d308d12dd535da182f4e7fba3f360fd7d70556dc
VoTanTien <115779921+VoTanTien@users.noreply.github.com>
2024-11-19 20:43:55 +0700

    Fix bug labeling image

---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+const Color purpleColor = Color(0xFFCCB3FC);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,11 @@
-import 'dart:html';
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_project/constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,116 +13,108 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Interpreter _interpreter;
-  List? _outputs;
-  File? _image;
-  bool _loading = false;
+  File? image;
+  late ImagePicker imagePicker;
+  late ImageLabeler labeler;
+  String results = "";
 
   @override
   void initState() {
     super.initState();
-    _loading = true;
+    imagePicker = ImagePicker();
+    ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.7);
+    labeler = ImageLabeler(options: options);
+  }
 
-    loadModel().then((value) {
+  chooseImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    if (selectedImage != null) {
+      image = File(selectedImage.path);
+      performImageLabeling();
       setState(() {
-        _loading = false;
+        image;
       });
-    });
-  }
-
-  loadModel() async {
-    try {
-      _interpreter =
-      await Interpreter.fromAsset('model.tflite'); // Sửa tên model nếu cần
-      print('Interpreter loaded successfully');
-    } catch (e) {
-      print('Failed to load model: $e');
     }
   }
 
-  classifyImage(File image) async {
-    if (_interpreter == null) {
-      print('Interpreter is not initialized');
-      return;
+  captureImage() async {
+    XFile? selectedImage =
+    await imagePicker.pickImage(source: ImageSource.camera);
+    if (selectedImage != null) {
+      image = File(selectedImage.path);
+      performImageLabeling();
+      setState(() {
+        image;
+      });
     }
+  }
 
-    img.Image imageInput = img.decodeImage(image.readAsBytesSync())!;
-    img.Image resizedImage = img.copyResize(
-        imageInput, width: 224, height: 224); // Điều chỉnh kích thước nếu cần
+  performImageLabeling() async{
+    results = "";
+    InputImage inputImage = InputImage.fromFile(image!);
 
-    var input = resizedImage.getBytes();
-    var output = List.filled(1 * 1001, 0).reshape(
-        [1, 1001]); // Điều chỉnh output tensor shape nếu cần
+    final List<ImageLabel> labels = await labeler.processImage(inputImage);
 
-    _interpreter.run(input, output);
-
-
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      results +=text+":    "+confidence.toStringAsFixed(2)+"\n";
+    }
     setState(() {
-      _loading = false;
-      _outputs = output;
+      results;
     });
   }
 
-
-  @override
-  void dispose() {
-    _interpreter.close();
-    super.dispose();
-  }
-
-  pickImage() async {
-    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) return null;
-    setState(() {
-      _loading = true;
-      _image = File(image.path as List<Object>);
-    });
-    classifyImage(_image!);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nhận diện hình ảnh'),
-      ),
-      body: _loading
-          ? Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(),
-      )
-          : Container(
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _image == null ? Container() : Image.file(_image!),
-            SizedBox(height: 20),
-            _outputs != null ?
-            Text(
-              //  "${_outputs![0][0]}", // Chỉ hiển thị giá trị đầu tiên
-              _outputs.toString(), // Hiển thị toàn bộ output
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  background: Paint()
-                    ..color = Colors.white),
-            )
-
-                : Container()
-          ],
+        appBar: AppBar(
+          title: const Text(
+            'Image Recognition',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: purpleColor,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: pickImage,
-        child: Icon(Icons.image),
-      ),
-    )
-    ,
-    );
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                image == null
+                    ? Icon(
+                        Icons.image_outlined,
+                        size: 150,
+                      )
+                    : Image.file(image!),
+                SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        chooseImage();
+                      },
+                      child: Text('Choose Image'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        captureImage();
+                      },
+                      child: Text('Capture Image'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Text(results)
+              ],
+            ),
+          ),
+        ));
   }
 }
