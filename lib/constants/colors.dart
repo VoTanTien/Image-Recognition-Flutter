3c53535b4e50ed1893bb9539d6004119d1c7f289
VoTanTien <115779921+VoTanTien@users.noreply.github.com>
2024-11-20 20:01:32 +0700

    Complete choose image and image recognition

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const Color purpleColor = Color(0xFFCCB3FC);
+const Color bgLightPurpleColor = Color(0xFFF4EFFD);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,9 @@ import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_project/constants/colors.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,15 +18,25 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   File? image;
   late ImagePicker imagePicker;
-  late ImageLabeler labeler;
+  late ImageLabeler imageLabeler;
   String results = "";
 
   @override
   void initState() {
     super.initState();
     imagePicker = ImagePicker();
-    ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.7);
-    labeler = ImageLabeler(options: options);
+    // ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.8);
+    // labeler = ImageLabeler(options: options);
+    loadModel();
+  }
+
+  loadModel()async{
+    final modelPath = await getModelPath('assets/ml/fruits.tflite');
+    final options = LocalLabelerOptions(
+      confidenceThreshold: 0.9,
+      modelPath: modelPath,
+    );
+    imageLabeler = ImageLabeler(options: options);
   }
 
   chooseImage() async {
@@ -40,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   captureImage() async {
     XFile? selectedImage =
-    await imagePicker.pickImage(source: ImageSource.camera);
+        await imagePicker.pickImage(source: ImageSource.camera);
     if (selectedImage != null) {
       image = File(selectedImage.path);
       performImageLabeling();
@@ -50,26 +63,38 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  performImageLabeling() async{
+  performImageLabeling() async {
     results = "";
     InputImage inputImage = InputImage.fromFile(image!);
 
-    final List<ImageLabel> labels = await labeler.processImage(inputImage);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
 
     for (ImageLabel label in labels) {
       final String text = label.label;
       final int index = label.index;
       final double confidence = label.confidence;
-      results +=text+":    "+confidence.toStringAsFixed(2)+"\n";
+      results += "Fruit name: "+text+"\n" + "Confidence: " + confidence.toStringAsFixed(2) + "\n";
     }
     setState(() {
       results;
     });
   }
 
+  Future<String> getModelPath(String asset) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$asset';
+    await Directory(dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(asset);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -81,39 +106,116 @@ class _HomeScreenState extends State<HomeScreen> {
           centerTitle: true,
           backgroundColor: purpleColor,
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                image == null
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Take a fruit picture',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: size.width,
+                height: size.height / 2,
+                decoration: BoxDecoration(
+                  color: bgLightPurpleColor,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: image == null
                     ? Icon(
                         Icons.image_outlined,
                         size: 150,
                       )
-                    : Image.file(image!),
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        chooseImage();
-                      },
-                      child: Text('Choose Image'),
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.file(
+                          image!,
+                          // fit: BoxFit.cover,
+                        ),
+                      ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      chooseImage();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(purpleColor),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        captureImage();
-                      },
-                      child: Text('Capture Image'),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.image_outlined,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Choose image',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      captureImage();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(purpleColor),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.camera_alt_outlined,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Take a picture',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Fruit in picture is:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: size.width,
+                decoration: BoxDecoration(
+                  color: bgLightPurpleColor,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      results,
+                      style: TextStyle(fontSize: 18),
                     ),
                   ],
                 ),
-                SizedBox(height: 5,),
-                Text(results)
-              ],
-            ),
+                padding: EdgeInsets.all(10),
+              )
+            ],
           ),
         ));
   }
